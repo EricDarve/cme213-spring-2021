f1f8e72fd95aaea1a5c15549b196e0a1e187a72d
Eric Darve <darve@stanford.edu>
2021-04-10 13:15:39 -0700

    code cleanup

--- Code/Lecture_05/reduction_array.cpp
+++ Code/Lecture_05/reduction_array.cpp
@@ -7,11 +7,11 @@ using namespace std;
 int main(void)
 {
     const int n = 512;
-    
+
     float b[n][n];
     float a[n];
 
-#pragma omp parallel for
+    #pragma omp parallel for
     for (int i = 0; i < n; i++)
     {
         a[i] = 0;
@@ -20,8 +20,7 @@ int main(void)
     }
 
     // Example 1: reduction with an array
-#pragma omp parallel for reduction(+ \
-                                   : a [0:n])
+    #pragma omp parallel for reduction(+: a [0:n])
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             a[j] += b[i][j];
@@ -38,23 +37,23 @@ int main(void)
     // Example 2: this loop does not require a reduction clause
 
     // Re-initialize
-#pragma omp parallel for
+    #pragma omp parallel for
     for (int i = 0; i < n; i++)
         a[i] = 0;
 
-#pragma omp parallel for
+    #pragma omp parallel for
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             a[i] += b[i][j];
 
     // Test
     for (int i = 0; i < n; i++)
-        {
+    {
         float sum = 0;
-        for (int j = 0; j < n; j++)        
+        for (int j = 0; j < n; j++)
             sum += b[i][j];
         assert(sum == a[i]);
-    }    
+    }
 
     cout << "PASS\n";
     return 0;
